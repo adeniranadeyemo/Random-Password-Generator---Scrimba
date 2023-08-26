1c7c7e01ac7c6bcb465684548595c71196f29977
adeniranadeyemo <niranadeniran1@gmail.com>
2023-08-26 20:57:42 +0100

    More styles and functionalities.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+'use strict';
 const characters = [
   'A',
   'B',
@@ -97,7 +98,27 @@ const passWord2El = document.getElementById('password2');
 const passwordGenButton = document.getElementById('button1');
 const savePasswordsButton = document.getElementById('button2');
 const clearPasswords = document.getElementById('button3');
+const innerPasswordCon = document.querySelector('.inner-saved-passwords-con');
+console.log(innerPasswordCon);
+//
+const passwordsArray = [];
+//
+const downArrow = document.querySelector('.down-arrow');
 
+let html;
+
+// Function to generate HTML for each password
+const generateHTML = function (password, i) {
+  html = `<div class="saved-password">
+              <div class="num-password">
+                <p class="password-numb">${i}.</p>
+                <p class="password">${password}</p>
+              </div>
+                <button class="delBtn">Delete</button>
+          </div>`;
+};
+
+// Function that generated random passwords from the characters array
 function randomPassword() {
   let array = Array(15)
     .fill()
@@ -106,6 +127,31 @@ function randomPassword() {
   return array.join('');
 }
 
+// Function that adds/pushes the passwords to the passwordsArray
+const addPasswords = function () {
+  if (
+    passWord1El.textContent &&
+    !passwordsArray.includes(passWord1El.textContent) &&
+    !passwordsArray.includes(passWord2El.textContent)
+  ) {
+    passwordsArray.push(passWord1El.textContent, passWord2El.textContent);
+  }
+
+  renderPasswords();
+};
+
+// Function that shows the passwords in the innerPasswordCon
+const renderPasswords = function () {
+  innerPasswordCon.innerHTML = '';
+
+  passwordsArray.forEach(function (password, index) {
+    generateHTML(password, index + 1);
+
+    innerPasswordCon.insertAdjacentHTML('beforeend', html);
+  });
+};
+
+// Event listener to display the passwords and the savePasswordsButton
 passwordGenButton.addEventListener('click', function () {
   passWord1El.textContent = randomPassword();
   passWord2El.textContent = randomPassword();
@@ -113,7 +159,56 @@ passwordGenButton.addEventListener('click', function () {
   savePasswordsButton.style.visibility = 'visible';
 });
 
+// Event listener to clear the displayed passwords
 clearPasswords.addEventListener('click', function () {
   passWord1El.textContent = passWord2El.textContent = '';
   savePasswordsButton.style.visibility = 'hidden';
 });
+
+// Event listener that triggers the addPasswords function
+savePasswordsButton.addEventListener('click', function () {
+  addPasswords();
+
+  passWord1El.textContent = passWord2El.textContent = '';
+});
+
+const togglePasswordCon = function () {
+  innerPasswordCon.classList.toggle('active');
+  downArrow.classList.toggle('rotate');
+};
+
+downArrow.addEventListener('click', togglePasswordCon);
+
+const passwordsMessage = document.querySelector('.saved-passwords-message');
+
+passwordsArray.length === 0
+  ? (passwordsMessage.textContent = 'No saved passwords yet.')
+  : '';
+
+// Function that deletes password
+function deletePassword(index) {
+  if (index !== -1) {
+    passwordsArray.splice(index, 1);
+    renderPasswords();
+  }
+}
+
+let delIndex;
+
+const findDelIndex = function (e) {
+  const pElem = e.target.parentNode.querySelector('.password');
+
+  const parentText = pElem.textContent;
+
+  delIndex = passwordsArray.findIndex((password) => password === parentText);
+};
+
+const del = function (e) {
+  if (e.target.classList.contains('delBtn')) {
+    findDelIndex(e);
+
+    deletePassword(delIndex);
+  }
+};
+
+innerPasswordCon.addEventListener('click', del);
